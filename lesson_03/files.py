b9dbe0e588965ec30ffa451f92b9b010a5e335aa
Anastasiia <ger.anastasia.a@gmail.com>
2023-08-15 17:21:49 +0300

    Exampels from classwork

---
+++ lesson_03/files.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+file = open(ROOT_DIR / "rockyou.txt")
+# text: str = file.readline()
+counter = 0
+
+while True:
+    try:
+        word = file.readline()
+        counter += 1
+        print(word)
+    except Exception:
+        break
+file.close()
+print(counter)
---
+++ lesson_03/iterators.py
@@ -0,0 +1,28 @@
+names: list[str] = ["John", "Mary"]
+
+_names = iter(names)
+print(_names.__next__())
+print(_names.__next__())
+print(_names.__next__())
+
+
+class Iterator:
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+
+instances = Iterator()
+
+for instance in instances:
+    print(instance)
+
+while True:
+    # name = _names.__next__()
+    try:
+        value = next(_names)
+        print(f"{value=}")
+    except StopIteration:
+        break
