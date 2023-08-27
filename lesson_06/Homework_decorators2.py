20caead332a4a4aef875e6436baf319e9c2b52ca
Anastasiia <ger.anastasia.a@gmail.com>
2023-08-27 11:17:50 +0300

    🩹 HW 5 part 2 is fixed

--- lesson_06/Homework_decorators2.py
+++ lesson_06/Homework_decorators2.py
@@ -10,7 +10,7 @@ def mask_data(target_key: str, replace_with: str = "*"):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            result[target_key] = replace_with * 3
+            result[target_key] = replace_with * len(result[target_key])
             return result
 
         return wrapper
