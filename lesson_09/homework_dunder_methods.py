293fd5c627ba5d9093780e5144acddd198ec45a4
Anastasiia <ger.anastasia.a@gmail.com>
2023-09-27 17:52:40 +0300

    🩹 HW 7 is fixed

--- lesson_09/homework_dunder_methods.py
+++ lesson_09/homework_dunder_methods.py
@@ -6,26 +6,38 @@ class Price:
         self.amount: int = amount
         self.currency: str = currency
 
-    def exchange(self, other: "Price"):
-        return int(self.amount / currency_dict[other.currency])
+    def exchange_other(self, other: "Price"):
+        return int(other.amount / currency_dict[other.currency])
+
+    def exchange_self(self):
+        return int(self.amount / currency_dict[self.currency])
 
     def __add__(self, other: "Price"):
+        print(self.exchange_self())
+        print(self.exchange_other(other))
+
         if self.currency == other.currency:
             return f"{self.amount + other.amount} {self.currency}"
+        elif self.currency == "USD" and other.currency != "USD":
+            return f"{self.amount + self.exchange_other(other)} USD"
+        elif self.currency != "USD" and other.currency == "USD":
+            return f"{self.exchange_self() + other.amount} USD"
         else:
-            # print(self.exchange(self))
-            # print(self.exchange(other))
-            return f"{self.exchange(self) + self.exchange(other)} USD"
+            return f"{self.exchange_self() + self.exchange_other(other)} USD"
 
     def __sub__(self, other: "Price"):
         if self.currency == other.currency:
             return f"{self.amount - other.amount} {self.currency}"
+        elif self.currency == "USD" and other.currency != "USD":
+            return f"{self.amount - self.exchange_other(other)} USD"
+        elif self.currency != "USD" and other.currency == "USD":
+            return f"{self.exchange_self() - other.amount} USD"
         else:
-            return f"{self.exchange(self) - self.exchange(other)} USD"
+            return f"{self.exchange_self() - self.exchange_other(other)} USD"
 
 
-price1 = Price(1000, "EUR")
-price2 = Price(1000, "USD")
+price1 = Price(1000, "GBP")
+price2 = Price(1000, "EUR")
 
 sum_p: Price = price1 + price2
 sub_p: Price = price1 - price2
